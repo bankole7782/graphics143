5c054711b47bf00bf962e3bf75d9db4ae5d57ce2
Bankole Ojo <ojobankole@gmail.com>
2024-10-04 14:05:15 +0100

    refactoring

--- shaders.go
+++ shaders.go
@@ -8,14 +8,6 @@ import (
 )
 
 const (
-	basicVertexShaderSource = `
-		#version 410
-		in vec3 vp;
-		void main() {
-			gl_Position = vec4(vp, 1.0);
-		}
-	`
-
 	textureVertexShaderSrc = `
 	#version 410
 
